0cbca5423bfbee3d451228270a7f2738eeb894ac
ashishk144 <ashishk1404@msitprogram.net>
2020-03-26 21:54:53 +0530

    Task5

--- Day3/application.py
+++ Day3/application.py
@@ -15,9 +15,9 @@ def default():
 
 @app.route('/success/<name>?<pwd>')
 def success(name, pwd):
-   print("name ", name)
-   print("pwd", pwd)
-   return 'welcome ' + name + " "+ pwd
+   # print("name ", name)
+   # print("pwd", pwd)
+   return render_template("/Success.html")
  
 @app.route('/register',methods = ['POST', 'GET'])
 def register():
@@ -27,14 +27,23 @@ def register():
       pwdcheck = request.form['psw-repeat']
       print(user,pwd,pwdcheck)
       # return render_template('/Register.html')
-      if(pwd == pwdcheck): 
-         usr = User(user_id = user, pwd = pwd, timestamp = datetime.now())
-         session.add(usr)
-         session.commit()
-      return redirect(url_for('success',name = user, pwd = pwd))
+      if(pwd == pwdcheck):
+         try:
+            usr = User(user_id = user, pwd = pwd, timestamp = datetime.now())
+            session.add(usr)
+            session.commit()
+            return redirect(url_for('success',name = user, pwd = pwd))
+         except Exception(e):
+            return redirect(url_for('error'))
+      return redirect(url_for('error'))
+
    else:
       return render_template('/Register.html')
  
+@app.route('/error')
+def error():
+   return render_template('/Fail.html')
+
 
 if __name__ == '__main__':
    app.run(debug = True)
